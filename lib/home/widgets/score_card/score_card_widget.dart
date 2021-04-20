4c480514c3697341e67692cc75d6cbbd9576cec5
Marcos <mrcsantos1@outlook.com>
2021-04-20 16:25:43 -0300

    dia 2

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -17,33 +17,37 @@ class _ScoreCardWidgetState extends State<ScoreCardWidget> {
         height: 136,
         decoration: BoxDecoration(
             color: AppColors.white, borderRadius: BorderRadius.circular(15)),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 1,
-              child: ChartWidget(),
-            ),
-            Expanded(
-                flex: 3,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 24),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Let's start!",
-                        style: AppTextStyles.heading,
-                      ),
-                      Text(
-                        "Complete the tasks and advance ...",
-                        style: AppTextStyles.body,
-                      ),
-                    ],
-                  ),
-                )),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: ChartWidget(),
+              ),
+              Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Let's start!",
+                          style: AppTextStyles.heading,
+                        ),
+                        Text(
+                          "Complete the tasks and advance ...",
+                          style: AppTextStyles.body,
+                        ),
+                      ],
+                    ),
+                  )),
+            ],
+          ),
         ),
       ),
     );
